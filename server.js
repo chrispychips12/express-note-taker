c31fded1d6d35a20935159994ae8634e89890aa4
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-08-17 11:39:47 +1000

    added API route to server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 // Added the following line to import the htmlRoutes.js file
 const htmlRoutes = require('./routes/htmlRoutes');
+// Added the following line to import the apiRoutes.js file
+const apiRoutes = require('./routes/apiRoutes');
 
 // Create an instance of an Express server
 const app = express();
@@ -16,6 +18,9 @@ app.use(express.static('public'));
 
 // Added the following line to use the htmlRoutes.js file
 app.use('/', htmlRoutes);
+
+// Added the following line to use the apiRoutes.js file
+app.use('/api', apiRoutes);
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
